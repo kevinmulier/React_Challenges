829d064abf1688c976e5162c65c5735785a6b2a9
kevinmulier <kevin.mulier@outlook.fr>
2024-02-17 13:20:19 +0100

    complete basic UI

--- BorderRadiusViewer/src/App.jsx
+++ BorderRadiusViewer/src/App.jsx
@@ -1,5 +1,48 @@
+import { useState } from 'react';
+
 function App() {
-  return <></>;
+  const [radius, setRadius] = useState({
+    topLeft: 0,
+    topRight: 0,
+    bottomLeft: 0,
+    bottomRight: 0,
+  });
+
+  const handleRadiusChange = (corner, value) => {
+    setRadius((v) => {
+      return { ...v, [corner]: value };
+    });
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center max-w-3xl gap-4 p-5 mx-auto">
+      <h1 className="text-3xl font-thin">Border Radius Viewer</h1>
+      <div className="flex justify-center w-full gap-2 mx-auto h-fit">
+        <div className="flex flex-col items-center justify-between w-fit">
+          <input
+            type="number"
+            className="text-center input input-bordered input-xs max-w-16"
+            onChange={(e) => handleRadiusChange('topLeft', e.target.value)}
+          />
+          <input
+            type="number"
+            className="text-center input input-bordered input-xs max-w-16"
+          />
+        </div>
+        <div className="w-6/12 h-60 bg-slate-500"></div>
+        <div className="flex flex-col items-center justify-between w-fit">
+          <input
+            type="number"
+            className="text-center input input-bordered input-xs max-w-16"
+          />
+          <input
+            type="number"
+            className="text-center input input-bordered input-xs max-w-16"
+          />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
