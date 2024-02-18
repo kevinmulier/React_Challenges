066f12ba73a987feda06e84a1006fda3af5409bc
kevinmulier <kevin.mulier@outlook.fr>
2024-02-18 23:14:48 +0100

    fix smaller screen style

--- LandingCountdown/src/components/TimeRemaining.jsx
+++ LandingCountdown/src/components/TimeRemaining.jsx
@@ -1,7 +1,7 @@
 const TimeRemaining = ({ value, unit }) => {
   return (
-    <div className="flex flex-col items-center justify-center max-sm:text-[0.75rem] max-md:text-[1rem] text-[1.5rem]">
-      <span className="font-mono font-bold text-[4em] leading-[0.75]">
+    <div className="flex flex-col items-center justify-center max-[500px]:text-[0.75rem] max-md:text-[1rem] text-[1.5rem]">
+      <span className="font-mono font-bold text-[3em] leading-[0.75]">
         {value}
       </span>
       <span className="text-[1em]">{unit}</span>
