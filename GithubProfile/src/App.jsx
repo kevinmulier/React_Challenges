8c21e588178c5b18af3991e5d8bc39ee04a91cd8
kevinmulier <kevin.mulier@outlook.fr>
2024-02-19 14:57:32 +0100

    add user fetching

--- GithubProfile/src/App.jsx
+++ GithubProfile/src/App.jsx
@@ -1,31 +1,104 @@
-import { Folder, Heart, MessageCircle, UserCheck, View } from 'lucide-react';
+import axios from 'axios';
+import { Folder, Search, UserCheck } from 'lucide-react';
+import { useState } from 'react';
 
 function App() {
+  const [username, setUsername] = useState('');
+  const [userInfos, setUserInfos] = useState(null);
+  const [pendingRequest, setPendingRequest] = useState(false);
+  const [fetchError, setFetchError] = useState('');
+
+  const searchUserInfos = async (username) => {
+    try {
+      setPendingRequest(true);
+      setFetchError('');
+      const infos = await axios.get(`https://api.github.com/users/${username}`);
+      setUserInfos(infos.data);
+      setPendingRequest(false);
+    } catch (e) {
+      setUserInfos(null);
+      setFetchError(`Couldn't find user ${username}`);
+      setPendingRequest(false);
+    }
+  };
+
+  if (pendingRequest) {
+    return (
+      <>
+        <section className="flex items-center justify-center w-full max-w-xl gap-8 p-4 sm:px-10 sm:py-6 rounded-2xl max-sm:flex-col bg-gradient-to-br from-base-100 to-base-300">
+          <div className="w-20 h-20 rounded-full aspect-square skeleton"></div>
+          <article className="flex flex-col gap-2">
+            <div className="w-24 h-4 skeleton"></div>
+            <div className="w-20 h-4 mb-4 skeleton"></div>
+            <div className="w-40 h-4 skeleton"></div>
+            <div className="w-40 h-4 skeleton"></div>
+          </article>
+        </section>
+      </>
+    );
+  }
+
+  if (userInfos === null) {
+    return (
+      <>
+        <div className="flex justify-center max-w-sm gap-3 mx-auto mb-5">
+          <input
+            type="text"
+            className="input input-bordered"
+            placeholder="Search by username..."
+            value={username}
+            onChange={(e) => setUsername(e.target.value.toLowerCase())}
+          />
+          <button
+            className="btn btn-outline"
+            onClick={() => searchUserInfos(username)}>
+            <Search />
+          </button>
+        </div>
+        {fetchError && <p className="alert alert-error">{fetchError}</p>}
+      </>
+    );
+  }
+
   return (
     <>
-      <section className="flex items-center justify-center max-w-xl gap-8 p-4 m-2 sm:px-10 sm:py-6 rounded-2xl max-sm:flex-col bg-gradient-to-br from-base-100 to-base-300">
-        <img
-          src=""
-          alt=""
-          className="sm:w-4/12 border-2 sm:border-[16px] rounded-full aspect-square border-neutral"
+      <div className="flex justify-center max-w-sm gap-3 mx-auto mb-5">
+        <input
+          type="text"
+          className="input input-bordered"
+          placeholder="Search by username..."
+          value={username}
+          onChange={(e) => setUsername(e.target.value.toLowerCase())}
         />
-        <article className="flex flex-col sm:w-8/12">
-          <h1 className="text-lg font-semibold">Shirley C. Ouzts</h1>
+        <button
+          className="btn btn-outline"
+          onClick={() => searchUserInfos(username)}>
+          <Search />
+        </button>
+      </div>
+      <section className="flex items-center justify-center w-full max-w-xl gap-8 p-4 sm:px-10 sm:py-6 rounded-2xl max-sm:flex-col bg-gradient-to-br from-base-100 to-base-300">
+        <img
+          src={userInfos.avatar_url}
+          alt={`${userInfos.login}'s avatar`}
+          className="max-w-52 sm:w-4/12 border-[10px] rounded-full aspect-square border-neutral"
+        />
+        <article className="flex flex-col max-sm:text-center sm:w-8/12">
+          <h1 className="text-lg font-semibold">{userInfos.login}</h1>
           <span className="text-sm font-thin text-opacity-70 text-info">
-            Frontend developer
+            {userInfos.company}
           </span>
-          <p className="mt-2 mb-6 text-xs">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem illo
-            suscipit, placeat odio, numquam.
-          </p>
-          <div className="flex gap-8 text-xs">
+          <span className="text-sm italic font-thin text-opacity-70 text-info">
+            {userInfos.location}
+          </span>
+          <p className="mt-2 mb-6 text-xs">{userInfos.bio}</p>
+          <div className="flex gap-8 text-xs max-sm:justify-center">
             <span className="flex items-center gap-1">
               <UserCheck />
-              2482
+              {userInfos.followers}
             </span>
             <span className="flex items-center gap-1">
               <Folder />
-              1473
+              {userInfos.public_repos}
             </span>
           </div>
         </article>
