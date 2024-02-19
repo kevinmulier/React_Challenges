eaaa645594003359a9ce8e414e0a3f445f86164b
kevinmulier <kevin.mulier@outlook.fr>
2024-02-19 13:50:41 +0100

    add custom theme

--- GithubProfile/tailwind.config.js
+++ GithubProfile/tailwind.config.js
@@ -7,4 +7,21 @@ export default {
     extend: {},
   },
   plugins: [daisyui],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          'primary': '#f3f4f6',
+          'secondary': '#b71f72',
+          'accent': '#ffffff',
+          'neutral': '#2A2565',
+          'base-100': '#422976',
+          'info': '#d9dddc',
+          'success': '#86efac',
+          'warning': '#fde68a',
+          'error': '#fca5a5',
+        },
+      },
+    ],
+  },
 };
